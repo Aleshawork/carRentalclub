7945fc4e459ae902946e5f3c3689b4a4bac246f2
Alexey <borisov_alexey_2002@mail.ru>
2022-03-09 08:51:12 +0300

    first commit

---
+++ src/main/java/service/work/carrentalclub/CarRentalclubApplication.java
@@ -0,0 +1,13 @@
+package service.work.carrentalclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalclubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalclubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/service/work/carrentalclub/CarRentalclubApplicationTests.java
@@ -0,0 +1,13 @@
+package service.work.carrentalclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalclubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
