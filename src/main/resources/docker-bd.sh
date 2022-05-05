cc535feb0becf385bcc501e9e3c3897d01607cad
Alexey <AMiBorisov@sberbank.ru>
2022-05-05 14:53:40 +0300

    Исправление тестов + sh на поднятие контейнера с PostgreSql

---
+++ src/main/resources/docker-bd.sh
@@ -0,0 +1,1 @@
+docker run --name db-local-dev -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=password -e POSTGRES_DB=car_rental_club -d postgres:13.3
--- src/test/java/service/work/carrentalclub/BaseAbstractTest.java
+++ src/test/java/service/work/carrentalclub/BaseAbstractTest.java
@@ -1,20 +1,31 @@
 package service.work.carrentalclub;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @TestPropertySource("/application-test.properties")
 @Transactional
 @Sql(scripts ={
+        "classpath:scripts/drop_schema.sql",
         "classpath:scripts/init_schema.sql",
         "classpath:scripts/test_init_car.sql",
         "classpath:scripts/test_init_user.sql",
