e010e5c77b899622f8ceeaff13f2facde0998d37
Alexey <borisov_alexey_2002@mail.ru>
2022-05-08 15:31:38 +0300

    Добавлены карточки на доступные для аренды авто. Добавлена страница подробной информации по авто

--- src/main/java/service/work/carrentalclub/controller/CarController.java
+++ src/main/java/service/work/carrentalclub/controller/CarController.java
@@ -11,6 +11,7 @@ import service.work.carrentalclub.model.TopOfCars;
 import service.work.carrentalclub.service.CarService;
 import service.work.carrentalclub.service.TopOfCarService;
 
+import javax.persistence.NoResultException;
 import java.util.List;
 import java.util.Set;
 
@@ -18,6 +19,8 @@ import java.util.Set;
 @Controller
 @RequestMapping("/car")
 public class CarController {
+
+    private final String IMAGE_URL = "https://fb.ru/media/i/2/8/9/8/7/i/28987.jpg";
 
     @Autowired
     private CarService carService;
@@ -33,11 +36,27 @@ public class CarController {
 
     }
 
+    @GetMapping("/{carId}")
+    public String findById(@PathVariable int carId, Model model) {
+        try {
+            model.addAttribute("car", carService.findById(carId));
+            String image = IMAGE_URL;
+            model.addAttribute("image", image);
+            return "car_info";
+        } catch(NoResultException ex) {
+            model.addAttribute("cars", carService.findFreeCar());
+            return "cars_carts";
+        }
+    }
+
     @GetMapping("/free")
     public String allFreeCars(Model model) {
         List<Car> freeCar = carService.findFreeCar();
         model.addAttribute("cars", freeCar);
-        return "cars";
+        //todo : add page url service by car mark
+        String url = IMAGE_URL;
+        model.addAttribute("page_url", url);
+        return "cars_carts";
     }
 
     @GetMapping("/lizing")
